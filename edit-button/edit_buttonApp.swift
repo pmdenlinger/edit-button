c0c4e18b315a5f6aa677b1eac00309a0d3f6725e
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 10:55:09 -0700

    Initial Commit

---
+++ edit-button/edit_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  edit_buttonApp.swift
+//  edit-button
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct edit_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
